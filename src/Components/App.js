a70ec16914e9b8bc52c6e698d64f3ce8798a9f38
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-22 13:18:18 +0530

    Before starting routing works in a aplication - 22-1-21

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import { BrowserRouter,Route,Link } from 'react-router-dom';
 const App = () =>{
-    return (<div>Hi</div>);
+    return (<div>
+       <BrowserRouter>
+        <div>
+            <Route path="/" exact component={One}/>   
+            <Route path="/two" component={Two}/>
+        </div>
+       </BrowserRouter> 
+    </div>);
 }
 
 export default App;
+
+
+const One = () => {
+    return <div>Welcome to Page One<br/><Link className="ui blue basic button" to="/two">To link page 2</Link></div>;
+}; 
+
+const Two = () => {
+    return <div>Welcome to  Page two<br/><Link className="ui blue basic button" to="/">To link page 1</Link></div>;
+}; 
